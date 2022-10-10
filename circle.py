d13c437dc9d92dbf0def05807e797171d6017cb2
SukitSeripanitkarn <lnwkit.se@gmail.com>
2022-10-10 13:38:49 -0400

    Circle class and its test

--- circle.py
+++ circle.py
@@ -2,7 +2,8 @@ import math
 
 class Circle(object):
 
-    def __init__(self, r):
+    def __init__(self, center,  r):
+        self.center = center
         self.radius = r
 
     def area_of_cir(self):
--- test_circle.py
+++ test_circle.py
@@ -6,6 +6,6 @@ def test_circle():
     x1, y1, x2, y2 = 0, 0, 3, 4
     p1 = Point(x1, y1)
     p2 = Point(x2, y2)
-    r = Line(p1, p2)
-    c = Circle(r)
-    assert "{:.2f}".format(c.area_of_cir()) == 78.53
+    r = Line(p1, p2).length()
+    c = Circle(p1, r)
+    assert "{:.2f}".format(c.area_of_cir()) == "78.54"
